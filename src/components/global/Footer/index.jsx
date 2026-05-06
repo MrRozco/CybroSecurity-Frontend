18c45530812d4e79e1f9438725b1aba583367da9
MrRozco <bryanorozco1997@hotmail.com>
2026-05-05 23:25:31 -0400

    footer fix

--- src/components/global/Footer/index.jsx
+++ src/components/global/Footer/index.jsx
@@ -60,7 +60,7 @@ const Footer = ({ data }) => {
         )}
 
         <p className={styles.footer__copyright}>
-          &copy; {new Date().getFullYear()} CybroSecurity. All rights reserved. Site made by NovaLink Web Solutions.
+          &copy; {new Date().getFullYear()} CybroSecurity. All rights reserved. Site made by <a href='https://appleseedsited.com' target="_blank" rel="noopener noreferrer">AppleSeed Web Solutions</a>.
         </p>
       </div>
     </footer>
